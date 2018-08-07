a6ee37c66530095e9ff491f4d136f619f7e385d7
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-08-07 12:55:12 +0530

    New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 import { FormsModule } from '@angular/forms';
+import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
+import { AppointmentServiceClient } from './services/appointment.service.client';
 
 import {
   MatFormFieldModule,
@@ -11,20 +13,26 @@ import {
   MatTableModule,
   MatListModule,
   MatDialogModule,
-  MatIconModule
+  MatIconModule,
+  MatDatepickerModule,
+  MatNativeDateModule
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { MakeAppointmentComponent } from './make-appointment/make-appointment.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ForgotPasswordComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    MakeAppointmentComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
@@ -37,9 +45,14 @@ import { FooterComponentComponent } from './footer-component/footer-component.co
     MatListModule,
     MatDialogModule,
     MatIconModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    DlDateTimePickerDateModule
   ],
-  providers: [],
+  providers: [
+    AppointmentServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-
+import { MakeAppointmentComponent } from './make-appointment/make-appointment.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'make-appointment', component: MakeAppointmentComponent },
+  { path: 'admin-page', component: AdminPageComponent },
   { path: '**', component: HomePageComponent }
 ];
 
