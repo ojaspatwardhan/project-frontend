0675f78628bd5b1a97f04df08333c9b44b5ae245
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-08-11 18:18:38 +0530

    Heroku deploy

--- src/app/services/email.service.client.ts
+++ src/app/services/email.service.client.ts
@@ -1,6 +1,9 @@
 export class EmailServiceClient {
+
+  //Heroku URL = https://lotus-lab-backend.herokuapp.com/
+  //Local URL = http://localhost:4000/
   sendForgotPasswordEmail(emailId, password) {
     console.log(emailId + " " + password)
-    return fetch("http://localhost:4000/api/email/" + emailId + "/" + password);
+    return fetch("https://lotus-lab-backend.herokuapp.com/api/email/" + emailId + "/" + password);
   }
 }
