9e4364b82171359d05bc6249621717db0571a183
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-08-12 19:16:41 +0530

    Heroku deploy

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -163,15 +163,15 @@ export class EditUserComponent implements OnInit {
 
   //Delete User
   deleteUser(id) {
-  console.log(id);
-  // this.userService.removeUser(id).then((response) => {
-  //   $("#userModal").modal("toggle");
-  //   this.userService.findAllUsers()
-  //   .then((users) => {
-  //     this.users = users;
-  //     console.log(this.users);
-  //   });
-  // });
+    console.log(id);
+    this.userService.removeUser(id).then((response) => {
+      $("#userModal").modal("toggle");
+      this.userService.findAllUsers()
+      .then((users) => {
+        this.users = users;
+        console.log(this.users);
+      });
+    });
   }
 
   //Get User
